d349a11339fe8991b3a39639a96e66aae89f6a5e
Sun <gajansuryan@gmail.com>
2020-05-05 20:24:41 -0400

    Models created Services microservice

---
+++ src/persistence/models.go
@@ -0,0 +1,32 @@
+package persistence
+
+import (
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Service struct {
+	ID        bson.ObjectId `bson:"_id"`
+	Name      string
+	Duration  int
+	StartDate int64
+	EndDate   int64
+	Cost      int64 //need to tie this with stylist level.
+}
+
+type Location struct {
+	ID        bson.ObjectId `bson:"_id"`
+	Name      string
+	Address   string
+	City      string
+	Country   string
+	OpenTime  int
+	CloseTime int
+	Services  []Service
+	Stylists  []Stylist
+}
+
+type Stylist struct {
+	Name     string `json:"name"`
+	Location string `json:"location,omitempty"`
+	Level    string `json:"stylistLevel"`
+}
